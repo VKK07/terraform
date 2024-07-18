3189c7b8e24ad1058ba4057ee4ac57bab63e0ae9
kiran kumar venkatesan <165485010+VKK07@users.noreply.github.com>
2024-07-18 11:54:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #provider.tf
 provider "aws" {
-  region     = "us-east-2"
+  region     = "us-east-1"
  }
 
 #vpc.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "subnet"
   }
@@ -99,10 +99,10 @@ variable "subnet_cidr" {
 
 # Creating EC2 instance
 resource "aws_instance" "terraform_instance" {
-  ami                         = "ami-0edf386e462400a51"
+  ami                         = "ami-0a0e5d9c7acc336f1"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "ohio"
+  key_name                    = "sai"
   vpc_security_group_ids      = ["${aws_security_group.tf_sg.id}"]
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
