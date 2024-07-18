e8d575f361af9da7ca969895e9218daef8e69384
kiran kumar venkatesan <165485010+VKK07@users.noreply.github.com>
2024-07-18 12:11:49 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,13 @@
-#!/bin/bash 
-sudo apt install -y docker 
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker
 sudo systemctl start docker
-sudo systemctl enable docker 
-sudo usermod -aG docker ubuntu 
-sudo chmod 666 /var/run/docker.sock 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-sudo chmod +x /usr/local/bin/docker-compose 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
-git clone https://github.com/VKK07/wordpress.git 
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+git clone https://github.com/VKK07/wordpress.git
 cd wordpress/
 docker-compose up -d
