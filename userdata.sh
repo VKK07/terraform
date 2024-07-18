41d9736ba0d0bc750f90cf1d4f623e07627d8449
kiran kumar venkatesan <165485010+VKK07@users.noreply.github.com>
2024-07-18 12:02:13 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ sudo apt install -y git
 sudo apt install -y docker 
 sudo systemctl start docker
 sudo systemctl enable docker 
-sudo usermod -aG docker ec2-user 
+sudo usermod -aG docker ubuntu 
 sudo chmod 666 /var/run/docker.sock 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
